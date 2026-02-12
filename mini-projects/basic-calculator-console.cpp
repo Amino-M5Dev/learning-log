89dcc58245e0ed614eb219a2f79b6cf618af636c
Amino Muhamad <aminuddin.m5dev@gmail.com>
2026-02-12 13:49:36 +0200

    Atualizar o basic-calculator-console.cpp
    add: comments.

--- mini-projects/basic-calculator-console.cpp
+++ mini-projects/basic-calculator-console.cpp
@@ -2,10 +2,12 @@
 #include <cmath>
 using namespace std;
 
+// Function prototypes (declarations)
 void menu();
 void input(double &x, double &y, char &oper);
 void operation(double x, double y, char oper);
 
+// Arithmetic operation functions
 double addition(double x, double y);
 double subtraction(double x, double y);
 double multiplication(double x, double y);
@@ -14,29 +16,38 @@ double modulo(double x, double y);
 
 int main() {
 
-    double n1, n2;
-    char oper;
-    char r = 'y';
-    int a;
+    double n1, n2;      // Variables to store the numbers
+    char oper;          // Variable to store the chosen operator
+    char r = 'y';       // Controls program repetition
+    int a = 0;          // Counter to control menu display
 
+    // Loop runs while user chooses 'y' or 'Y'
     while (r == 'y' || r == 'Y') {
 
-        if(a==0)
+        // Show menu only the first time
+        if(a == 0)
             menu();
+
+        // Get user input (numbers and operator)
         input(n1, n2, oper);
 
         cout << "\nResult:\n";
+
+        // Perform selected operation
         operation(n1, n2, oper);
 
+        // Ask user if they want to continue
         cout << "\nDo you want to perform another operation? (Y / N): ";
         cin >> r;
         cout << endl;
-        a++;
+
+        a++; // Increment counter
     }
 
     return 0;
 }
 
+// Displays calculator menu
 void menu() {
     cout << "==========================\n";
     cout << "==== Basic Calculator ====\n";
@@ -50,6 +61,8 @@ void menu() {
     cout << "%  Remainder\n\n";
 }
 
+// Gets user input using references
+// Reference (&) allows modifying original variables
 void input(double &x, double &y, char &oper) {
 
     cout << "Enter #1: ";
@@ -62,6 +75,7 @@ void input(double &x, double &y, char &oper) {
     cin >> y;
 }
 
+// Chooses which mathematical operation to execute
 void operation(double x, double y, char oper) {
 
     switch (oper) {
@@ -82,6 +96,7 @@ void operation(double x, double y, char oper) {
             break;
 
         case '/':
+            // Prevent division by zero
             if (y == 0)
                 cout << "Error: division by zero.\n";
             else
@@ -90,6 +105,7 @@ void operation(double x, double y, char oper) {
             break;
 
         case '%':
+            // Prevent modulo by zero
             if (y == 0)
                 cout << "Error: modulo by zero.\n";
             else
@@ -102,22 +118,27 @@ void operation(double x, double y, char oper) {
     }
 }
 
+// Returns the sum of two numbers
 double addition(double x, double y) {
     return x + y;
 }
 
+// Returns the difference between two numbers
 double subtraction(double x, double y) {
     return x - y;
 }
 
+// Returns the product of two numbers
 double multiplication(double x, double y) {
     return x * y;
 }
 
+// Returns the division of two numbers
 double division(double x, double y) {
     return x / y;
 }
 
+// Returns the remainder using fmod (for double values)
 double modulo(double x, double y) {
     return fmod(x, y);
 }
